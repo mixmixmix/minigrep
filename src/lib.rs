5310fcbc8642eb6c0c9a2ba91402e2b81748b490
M Kunde <mikolaj.kundegorski@gmail.com>
2022-05-09 17:09:47 +0600

    tested things so far

--- src/lib.rs
+++ src/lib.rs
@@ -33,3 +33,54 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
     println!("We got the following:\n{}", contents);
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+// #[test]
+//     fn result_one(){
+//         let query = "duck";
+//         let contents = "\
+// Bloody hell,
+// living duck,
+// road of vice!";
+
+//         assert_eq!(
+//             vec!["living duck,"],
+//             search(query,contents)
+//         );
+//     }
+
+    //You might ask my friend why those arrays of string are so weird. It is Rust. But also it is because we decided to use Strings, and clone them when passing arguments, instead of having &str so that we don't have to "worry" about lifetimes. In the future we'll be proper!!! ;)
+    #[test]
+    fn config_test(){
+        assert!(Config::new(&["too".to_string(), "little".to_string()]).is_err());
+    }
+
+    #[test]
+    fn run_test_good(){
+        let config = Config::new(&["minigrep_target_name".to_string(),
+                                   "query".to_string(),
+                                   "files/haystack.txt".to_string()]).unwrap();
+
+        run(config).unwrap();
+    }
+
+    #[test]
+    fn run_test_bad(){
+        let config = Config::new(&["minigrep_target_name".to_string(),
+                                   "query".to_string(),
+                                   "NONEXISTENT_FILE.txt".to_string()]).unwrap();
+        assert!(run(config).is_err());
+    }
+
+    #[test]
+    #[should_panic]
+    fn run_pass_err(){
+        let config = Config::new(&["minigrep_target_name".to_string()
+                                   ]);
+        run(config.unwrap()).unwrap(); //this little unwrap will panic because we are paasing there an unhandled error!
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    println!("{:?}",args);
     let config = Config::new(&args).unwrap_or_else(|err|{
         println!("problem in config creation: {}", err);
         process::exit(1);
