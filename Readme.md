86925704a9eb4868d445b2dba719f3e3572cb9aa
M Kunde <mikolaj.kundegorski@gmail.com>
2022-05-09 18:49:47 +0600

    use env variable

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### Rust book
+All from chapter 12 of Rustbook
+
+### Bonus
+Try env variable `CASE_SENSITIVE` binary flag `0|1`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use envmnt;
 
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -15,10 +17,35 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = envmnt::is_or("CASE_SENSITIVE", false);
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive})
     }
 }
+
+pub fn search<'mlifetime>(query: &str, contents: &'mlifetime str) -> Vec<&'mlifetime str>{
+    let mut foundlines = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            foundlines.push(line);
+        }
+    }
+    foundlines
+}
+
+pub fn search_insensitive<'mlifetime>(query: &str, contents: &'mlifetime str) -> Vec<&'mlifetime str>{
+    let mut foundlines = Vec::new();
+    let query = query.to_lowercase(); //to_lowercase
+
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query){
+            foundlines.push(line);
+        }
+    }
+    foundlines
+}
+
 
 pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
 
@@ -30,7 +57,18 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
     myfile
         .read_to_string(&mut contents)?;
 
-    println!("We got the following:\n{}", contents);
+    let myres = if config.case_sensitive {
+        search(&config.query,&contents)
+    } else {
+        search_insensitive(&config.query,&contents)
+    };
+
+
+    for line in myres{
+        println!("{}",line);
+    }
+
+
     Ok(())
 }
 
@@ -38,19 +76,38 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
 mod test {
     use super::*;
 
-// #[test]
-//     fn result_one(){
-//         let query = "duck";
-//         let contents = "\
-// Bloody hell,
-// living duck,
-// road of vice!";
+#[test]
+    fn result_one(){
+        let query = "duck";
+        //interesting, this one backlash is to make sure the contents do not start with an empty line
+        //from https://github.com/rust-lang/regex/issues/107
+        //Line-break characters are allowed in string literals. Normally they represent themselves (i.e. no translation), but as a special exception, when a U+005C character \ occurs immediately before the newline, the U+005C character, the newline, and all whitespace at the beginning of the next line are ignored. Thus a and b are equal:
+        let contents = "\
+Bloody hell,
+living duck,
+road of vice,
+also Duck";
 
-//         assert_eq!(
-//             vec!["living duck,"],
-//             search(query,contents)
-//         );
-//     }
+        assert_eq!(
+            vec!["living duck,"],
+            search(query,contents)
+        );
+    }
+
+    #[test]
+    fn result_insensitive(){
+        let query = "duck";
+        let contents = "
+Bloody hell,
+living Duck,
+road of vice,
+also duck";
+
+        assert_eq!(
+            vec!["living Duck,","also duck"],
+            search_insensitive(query,contents)
+        );
+    }
 
     //You might ask my friend why those arrays of string are so weird. It is Rust. But also it is because we decided to use Strings, and clone them when passing arguments, instead of having &str so that we don't have to "worry" about lifetimes. In the future we'll be proper!!! ;)
     #[test]
